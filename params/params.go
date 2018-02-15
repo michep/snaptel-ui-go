7b68984a597d10facf0b42cba7040a545b4bee1c
Mike Chepaykin <mchepaykin@mfms.ru>
2018-02-15 13:12:06 +0300

    fix style

--- params/params.go
+++ params/params.go
@@ -1,8 +1,6 @@
 package params
 
 type Params struct {
-	Debug        bool   `short:"d" long:"debug" description:"enable debug logging"`
-	PprofAddr    string `short:"p" long:"pprof" description:"enable pprof, listen on this address"`
 	ListenAddr   string `short:"l" long:"listen" required:"true" description:"snaptel-ui listen address"`
 	SnapListFile string `short:"f" long:"file" required:"true" description:"file with list of snap-telemetry URLs"`
 }
